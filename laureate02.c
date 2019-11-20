61b9ac091d5e1f1fba9fca857c908f175271d0c7
wim <wiliam.tae@gmail.com>
2019-11-20 17:43:48 -0200

    struct of an car created

--- laureate02.c
+++ laureate02.c
@@ -3,30 +3,34 @@
 int main()
 {
 
+  struct car {
+    char brand[10];
+    int model;
+    char letters[3];
+    int numbers;
+  };
+
+ struct car car0;
+
   /*marca do veículo*/
 
-  char brand[10];
   printf("insert the brand of the car: \n");
-  scanf("%s", brand);
-  printf("your car brand is %s", brand);
+  scanf("%s", car0.brand);
+  printf("your car brand is %s", car0.brand);
 
   /*model age*/
 
-  int model;
   printf("How your car model age? \n");
-  scanf("%d", &model);
-  printf("your car model is %d \n", model);
+  scanf("%d", &car0.model);
+  printf("your car model is %d \n", car0.model);
 
   /*identifier*/
 
-  char letters[3];
-  int numbers;
   printf("tell me your car identifier first 3 letters \n");
-  scanf("%s", letters);
+  scanf("%s", car0.letters);
   printf("tell now the last 4 numbers \n");
-  scanf("%d", &numbers);
-  printf("your identifier is: %s-%d ", letters, numbers);
-
+  scanf("%d", &car0.numbers);
+  printf("your identifier is: %s-%d ", car0.letters, car0.numbers);
 
 
   return 0;
